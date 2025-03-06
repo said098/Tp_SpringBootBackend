871b0ac293a99b2f462db9f2ac0c3f7d7931e8a7
saidkamal <saidkamalshinwari098@gmail.com>
2025-03-06 14:01:29 +0100

    Initial commit

---
+++ src/main/java/org/univ_paris8/iut/montreuil/s5a01_e_stage_montreuilbackend/tp_springboot/TpSpringBootApplication.java
@@ -0,0 +1,14 @@
+package org.univ_paris8.iut.montreuil.s5a01_e_stage_montreuilbackend.tp_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpSpringBootApplication.class, args);
+        System.out.println("Application Démarée");
+    }
+
+}
