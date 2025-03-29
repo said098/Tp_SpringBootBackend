1290e16a9e46529efe7ba1cddfc2c7ab0aa89da1
saidkamal <saidkamalshinwari098@gmail.com>
2025-03-29 05:22:01 +0100

    ajouter les infos pour mdp et username

--- src/main/java/org/univ_paris8/iut/montreuil/s5a01_e_stage_montreuilbackend/tp_springboot/TpSpringBootApplication.java
+++ src/main/java/org/univ_paris8/iut/montreuil/s5a01_e_stage_montreuilbackend/tp_springboot/TpSpringBootApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TpSpringBootApplication {
     public static void main(String[] args) {
         SpringApplication.run(TpSpringBootApplication.class, args);
-        System.out.println("Pour les information j'ai dé");
+        System.out.println("juste pour les information j'ai désactive le security de springboot " +
+                "\n Le user Name et mot de passe sont dans fichier config.properties" +
+                "\n voici le mot de passe et username : tpspringboot");
         System.out.println("Application Démarée");
 
     }
